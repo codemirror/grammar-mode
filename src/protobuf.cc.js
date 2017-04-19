32fe0fcd8d9d24615c739bb402096eddb045ec41
Marijn Haverbeke <marijn@haverbeke.nl>
2017-04-19 15:15:03 +0200

    Separate the failure stack from the regular return stack

--- src/protobuf.cc.js
+++ src/protobuf.cc.js
@@ -20,93 +20,14 @@ class Rule {
 
 const F = {F: true}, C = {C: true}
 
-class Matcher {
-  constructor(input) {
-    this.pos = 0
-    this.input = input
-    this.stack = []
-    this.callee = null
-  }
-
-  re(re) {
-    let match = re.exec(this.input.slice(this.pos))
-    if (match) this.pos += match[0].length
-    return match || F
-  }
-
-  str(string) {
-    if (this.input.slice(this.pos, this.pos + string.length) == string)
-      this.pos += string.length
-    else
-      return F
-  }
-
-  eof() {
-    return this.pos == this.input.length ? null : F
-  }
-
-  backtrack(pos) {
-    this.pos = pos
-  }
-
-  group(str) {
-    if (this.pos < this.input.length && str.indexOf(this.input[this.pos]) > -1)
-      this.pos++
-    else
-      return F
-  }
-
-  call(rule, success, failure) {
-    this.stack.push(success, failure)
-    this.callee = rule
-    return C
-  }
-
-  callWith(rule, arg, success, failure) {
-    this.stack.push(success, failure, arg)
-    this.callee = rule
-    return C
-  }
-
-  exec(rule) {
-    this.stack.length = 0
-    for (let next = rule.code, arg = null;;) {
-      let result = next(this, arg)
-      if (result == F) {
-        if (this.stack.length == 0) return F
-        next = this.stack.pop()
-        this.stack.pop()
-        arg = null
-      } else if (result == C) {
-        next = this.callee.code
-        arg = null
-      } else {
-        if (this.stack.length == 0) return result
-        this.stack.pop()
-        next = this.stack.pop()
-        arg = result
-      }
-    }
-  }
-}
-
 const space = " \t\r\n"
 
-function fail_0() {
-  return F
-}
-
-function fail_1(m) {
-  m.stack.pop()
-  return F
-}
-
-function succeed_0(_, v) {
+function succeed(_, v) {
   return v
 }
 
-function succeed_1(m, v) {
-  m.stack.pop()
+function succeed_drop(m, v) {
+  m.backtrackStack.pop()
   return v
 }
 
@@ -118,76 +39,75 @@ function Program_0(m) {
 }
 
 function Program_1(m) {
-  return m.eof() == F ? m.call(Statement, Program_1, fail_0) : null
+  return m.eof() == F ? m.call(Statement, Program_1) : null
 }
 
 const Statement = new Rule("Statement", Statement_0)
 
 function Statement_0(m) {
-  m.stack[m.stack.length] = m.pos
-  return m.callWith(kw, "package", Statement_package_1, Statement_message)
+  m.setBacktrack(Statement_message)
+  return m.callWith(kw, "package", Statement_package_1)
 }
 
 function Statement_package_1(m) {
-  return m.call(identifier, Statement_package_2, Statement_message)
+  return m.call(identifier, Statement_package_2)
 }
 
 function Statement_package_2(m) {
-  return m.callWith(p, ";", succeed_1, Statement_message)
+  return m.callWith(p, ";", succeed_drop)
 }
 
 function Statement_message(m) {
-  m.backtrack(m.stack.pop())
-  return m.callWith(kw, "message", Statement_message_1, fail_0)
+  return m.callWith(kw, "message", Statement_message_1)
 }
 
 function Statement_message_1(m) {
-  return m.call(identifier, Statement_message_2, fail_0)
+  return m.call(identifier, Statement_message_2)
 }
 
 function Statement_message_2(m) {
-  return m.callWith(p, "{", Statement_message_3, fail_0)
+  return m.callWith(p, "{", Statement_message_3)
 }
 
 function Statement_message_3(m) {
-  return m.callWith(p, "}", succeed_0, Statement_message_4)
+  m.setBacktrack(Statement_message_4)
+  return m.callWith(p, "}", succeed_drop)
 }
 
 function Statement_message_4(m) {
-  return m.call(Field, Statement_message_3, fail_0)
+  return m.call(Field, Statement_message_3)
 }
 
 const Field = new Rule("Field", Field_0)
 
 function Field_0(m) {
-  m.stack[m.stack.length] = m.pos
-  return m.call(modifier, Field_1, Field_2)
+  m.setBacktrack(Field_2)
+  return m.call(modifier, Field_1)
 }
 
 function Field_1(m) {
-  m.stack.pop()
+  m.backtrackStack.pop()
   return Field_0(m)
 }
 
 function Field_2(m) {
-  m.backtrack(m.stack.pop())
-  return m.call(type, Field_3, fail_0)
+  return m.call(type, Field_3)
 }
 
 function Field_3(m) {
-  return m.call(identifier, Field_4, fail_0)
+  return m.call(identifier, Field_4)
 }
 
 function Field_4(m) {
-  return m.callWith(p, "=", Field_5, fail_0)
+  return m.callWith(p, "=", Field_5)
 }
 
 function Field_5(m) {
-  return m.call(number, Field_6, fail_0)
+  return m.call(number, Field_6)
 }
 
 function Field_6(m) {
-  return m.callWith(p, ";", succeed_0, fail_0)
+  return m.callWith(p, ";", succeed)
 }
 
 const identifier = new Rule("identifier", identifier_0, true)
@@ -203,17 +123,17 @@ function identifier_0(m) {
 const kw = new Rule("kw", kw_0, true)
 
 function kw_0(m) {
-  return m.call(identifier, kw_1, fail_1)
+  return m.call(identifier, kw_1)
 }
 
 function kw_1(m, ident) {
-  return m.stack.pop() == ident ? null : F
+  return m.arg == ident ? null : F
 }
 
 const type = new Rule("type", type_0, true)
 
 function type_0(m) {
-  return m.call(identifier, type_1, fail_0)
+  return m.call(identifier, type_1)
 }
 
 function type_1(_, ident) {
@@ -223,7 +143,7 @@ function type_1(_, ident) {
 const modifier = new Rule("modifier", modifier_0, true)
 
 function modifier_0() {
-  return m.call(identifier, modifier_1, fail_0)
+  return m.call(identifier, modifier_1)
 }
 
 function modifier_1(_, ident) {
@@ -233,7 +153,7 @@ function modifier_1(_, ident) {
 const p = new Rule("p", p_0, true)
 
 function p_0(m) {
-  if (m.str(m.stack.pop()) == F) return F
+  if (m.str(m.arg) == F) return F
   while (m.group(space) != F) {}
 }
 
@@ -255,68 +175,145 @@ class Match {
   }
 }
 
-class ModeMatcher extends Matcher {
+class Frame {
+  constructor(match, arg, next) {
+    this.match = match
+    this.arg = arg
+    this.next = next
+  }
+}
+
+class BacktrackFrame {
+  constructor(pos, isLookahead, frameDepth, next) {
+    this.pos = pos
+    this.isLookahead = isLookahead
+    this.frameDepth = frameDepth
+    this.next = next
+  }
+}
+
+class ModeMatcher {
   constructor(input) {
-    super(input)
+    this.pos = 0
+    this.input = input
+    // :: [Frame]
+    this.stack = []
+    // :: [BacktrackFrame]
+    this.backtrackStack = []
+    this.callee = null
     this.cache = []
-    this.currentMatch = null
+    this.frontierState = null
+    this.frontierPos = -1
   }
 
-  isCached(rule, arg) {
+  get currentMatch() {
+    for (let i = this.stack.length - 1; i >= 0; i--) {
+      let m = this.stack[i].match
+      if (m) return m
+    }
+  }
+
+  get arg() {
+    return this.stack[this.stack.length - 1].arg
+  }
+
+  re(re) {
+    let match = re.exec(this.input.slice(this.pos))
+    if (match) this.pos += match[0].length
+    return match || F
+  }
+
+  str(string) {
+    if (this.input.slice(this.pos, this.pos + string.length) == string)
+      this.pos += string.length
+    else
+      return F
+  }
+
+  eof() {
+    return this.pos == this.input.length ? null : F
+  }
+
+  setBacktrack(next, lookahead) {
+    this.backtrackStack.push(new BacktrackFrame(this.pos, lookahead, this.stack.length, next))
+  }
+
+  group(str) {
+    if (this.pos < this.input.length && str.indexOf(this.input[this.pos]) > -1)
+      this.pos++
+    else
+      return F
+  }
+
+  getMatch(rule, arg) {
     let cached = this.cache[this.pos]
     if (cached) for (let i = 0; i < cached.length; i++)
       if (cached[i].rule === rule && cached[i].arg === arg) return cached[i]
   }
 
-  call(rule, success, failure) {
-    return this.callWith(rule, F, success, failure)
+  call(rule, next) {
+    return this.callWith(rule, F, next)
   }
 
-  callWith(rule, arg, success, failure) {
+  callWith(rule, arg, next) {
+    console.log("CALL", rule.name, "with", arg, "@", this.pos)
     let match
     if (rule.cached) {
       let argVal = arg == F ? null : arg
-      let cached = this.isCached(rule, argVal)
+      let cached = this.getMatch(rule, argVal)
       if (cached) {
         cached.parent = this.currentMatch
         this.pos = cached.end
-        return success(this, cached.value)
+        return next(this, cached.value)
       }
-      this.currentMatch = match = new Match(rule, argVal, this.pos, this.currentMatch)
+      match = new Match(rule, argVal, this.pos, this.currentMatch)
     }
-    let s = this.stack, l = s.length
-    s[l++] = match; s[l++] = success; s[l++] = failure
-    if (arg != F) s[l++] = arg
+    this.stack.push(new Frame(match, arg, next))
     this.callee = rule
     return C
   }
+/*
+  maybePreserveFrontier() {
+    if (this.pos <= this.frontierPos) return
+    let last = this.backtrackStack[this.backtrackStack.length - 1]
+    if (last < 0 || last >= this.pos - MIN_SAVE_DISTANCE) return
+    console.log("storing frontier @", this.pos)
+    this.frontierState = this.stack.slice()
+    this.frontierPos = this.pos
+  }
+
+  restoreFrontier() {
+    console.log("restore frontier from", this.pos, "to", this.frontierPos)
+    this.state = this.frontierState
+    this.pos = this.frontierPos
+    // FIXME more state mangling?
+  }*/
 
   exec(startRule, upto) {
     let result = C
     this.callee = startRule
     for (;;) {
-      if (result == C) {
+      if (result === C) {
         result = this.callee.code(this)
-      } else if (this.stack.length == 0) { // FIXME fall back to tokenizing
+      } else if (result === F) {
+        let backtrack = this.backtrackStack.pop()
+        // FIXME may be null
+        this.pos = backtrack.pos
+        this.stack.length = backtrack.frameDepth
+        result = backtrack.next(this)
+      } else if (this.stack.length == 0) {
         if (this.pos >= upto) return
-        result = C
         this.callee = startRule
+        result = C
       } else {
-        let onFail = this.stack.pop(), onSucc = this.stack.pop(), match = this.stack.pop()
+        if (this.pos > upto) return
+        let frame = this.stack.pop(), match = frame.match
         if (match) {
-          this.currentMatch = match.parent
-          if (result != F) {
-            match.end = this.pos
-            match.value = result
-            ;(this.cache[match.start] || (this.cache[match.start] = [])).push(match)
-          }
+          match.end = this.pos
+          match.value = result
+          ;(this.cache[match.start] || (this.cache[match.start] = [])).push(match)
         }
-        if (result == F) {
-          result = onFail(this)
-        } else {
-          if (this.pos > upto) return
-          result = onSucc(this, result)
-        }
+        result = frame.next(this, result)
       }
     }
   }
