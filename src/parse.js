458af77db4de5641bc2a22fb28fc569b5de69dbc
Marijn Haverbeke <marijn@haverbeke.nl>
2017-04-26 14:22:31 +0200

    Move old parser to parse_old.js

--- src/parse.js
+++ src/parse.js
@@ -1,244 +1,131 @@
-const acorn = require("acorn")
+const {tokTypes: tt, parse, plugins} = require("acorn")
 
-exports.parseGrammars = function(file, fileName) {
-  let re = /\n\s*grammar\s+([\w$]+)\s+\{/g, match, grammars = {}
-  while (match = re.exec(file)) {
-    let parser = new GrammarParser(file, fileName, match.index + match[0].length)
-    let rules = parser.parseRuleSet(), grammar = {
-      name: match[1],
-      firstRule: rules.length && rules[0].name,
-      rules: {},
-      start: match.index + 1, end: parser.pos
-    }
-    rules.forEach(r => grammar.rules[r.name] = r)
-    grammars[match[1]] = grammar
+function parseGrammar(p) {
+  let node = p.startNode()
+  p.next()
+  node.name = p.parseIdent()
+  node.rules = []
+  node.whitespace = null
+  p.expect(tt.braceL)
+  while (!p.eat(tt.braceR)) {
+    if (p.eatContextual("rules"))
+      parseRules(p, node.rules, true)
+    else if (p.eatContextual("tokens"))
+      parseRules(p, node.rules, false)
+    else if (p.eatContextual("helpers"))
+      parseRules(p, node.rules, false)
+    else if (p.eatContextual("whitespace"))
+      node.whitespace = parseRule(p)
   }
-  return grammars
+  return p.finishNode(node, "GrammarDeclaration")
 }
 
-const acornOptions = {allowReturnOutsideFunction: true}
-
-class GrammarParser {
-  constructor(input, fileName, pos) {
-    this.input = input
-    this.fileName = fileName
-    this.pos = pos
-    this.tokenStart = pos
-    this.tokenType = this.tokenValue = null
-    this.nextToken()
+function parseRules(p, rules, lexical) {
+  p.expect(tt.braceL)
+  while (!p.eat(tt.braceR)) {
+    let node = p.startNode()
+    node.lexical = lexical
+    node.name = p.parseIdent()
+    node.expr = parseRule(p)
+    if (p.eat(tt.arrow))
+      p.parseFunctionBody(node, true)
+    rules.push(p.finishNode(node, "RuleDeclaration"))
   }
+}
 
-  skipSpace() {
-    while (this.pos < this.input.length) {
-      let next = this.input[this.pos]
-      if (next == "/" && this.input[this.pos + 1] == "/") {
-        this.pos += 2
-        while (this.pos < this.input.length && !isNewline(this.input[this.pos])) this.pos++
-      } else if (/\s/.test(next)) {
-        this.pos++
-      } else {
-        break
-      }
-    }
-  }
+function parseRule(p) {
+  p.expect(tt.braceL)
+  let expr = parseExprChoice(p)
+  p.expect(tt.braceR)
+  return expr
+}
 
-  nextToken() {
-    this.skipSpace()
-    if (this.pos == this.input.length) {
-      this.tokenType = "eof"
-      this.tokenValue = null
-      return
-    }
-    let start = this.tokenStart = this.pos, next = this.input[this.pos++]
-    if (identifierChar(next)) {
-      while (this.pos < this.input.length && identifierChar(this.input[this.pos])) this.pos++
-      this.tokenType = "identifier"
-      this.tokenValue = this.input.slice(start, this.pos)
-    } else if (/[\[\]{}()?|*+:!=]/.test(next)) {
-      this.tokenType = "punctuation"
-      this.tokenValue = next
-    } else if (next == '"' || next == "'") {
-      let quote = next
-      for (;;) {
-        if (this.pos == this.input.length) this.raise("Unterminated string literal")
-        next = this.input[this.pos++]
-        if (next == "\\" && this.pos < this.input.length) this.pos++
-        if (next == quote) break
-      }
-      this.tokenType = "string"
-      this.tokenValue = JSON.parse(this.input.slice(start, this.pos))
-    } else if (next == "/") {
-      for (;;) {
-        if (this.pos == this.input.length) this.raise("Unterminated regexp")
-        next = this.input[this.pos++]
-        if (next == "\\" && this.pos < this.input.length) this.pos++
-        if (next == "/") break
-      }
-      let re = this.input.slice(start + 1, this.pos - 1), flagStart = this.pos
-      while (this.pos < this.input.length && /\w/.test(this.input[this.pos])) this.pos++
-      this.tokenType = "regexp"
-      this.tokenValue = new RegExp(`^(?:${re})`, this.input.slice(flagStart, this.pos))
-    } else {
-      this.raise(`Unexpected character '${next}'`)
-    }
-  }
-
-  ident() {
-    if (this.tokenType != "identifier") this.raise("Expected identifier")
-    let value = this.tokenValue
-    this.nextToken()
-    return value
-  }
-
-  is(punc) {
-    return this.tokenType == "punctuation" && this.tokenValue == punc
-  }
-
-  eat(punc) {
-    if (this.is(punc)) {
-      this.nextToken()
-      return true
-    } else {
-      return false
-    }
-  }
-
-  expect(punc) {
-    if (!this.eat(punc)) this.raise(`Expected '${punc}'`)
-  }
-
-  find(pos) {
-    let line = 1, lineStart = 0
-    for (;;) {
-      let next = this.input.indexOf("\n", lineStart)
-      if (next == -1 || next >= pos) break
-      line++
-      lineStart = next + 1
-    }
-    return {line, ch: pos - lineStart}
-  }
-
-  raise(err) {
-    let {line, ch} = this.find(this.tokenStart)
-    throw new SyntaxError(`${err} (${this.fileName ? this.fileName + " " : ""}${line}:${ch})`)
-  }
-
-  jsBlock() {
-    if (!this.is("{")) this.raise("Expected JavaScript block")
-    let p = new acorn.Parser(acornOptions, this.input, this.pos - 1)
-    p.nextToken()
-    let block = p.parseBlock()
-    this.pos = p.start
-    this.nextToken()
-    return {ast: block.body, string: this.input.slice(block.start + 1, block.end - 1)}
-  }
-
-  jsArgs() {
-    let p = new acorn.Parser(acornOptions, this.input, this.pos - 1)
-    p.nextToken()
-    let array = p.parseExprAtom()
-    this.pos = p.start
-    this.nextToken()
-    return {ast: array.elements, string: this.input.slice(array.start + 1, array.end - 1)}
-  }
-
-  parseExpression() {
-    let start = this.parseSequenceExpression()
-    if (!this.eat("|")) return start
-    let choices = [start]
-    do { choices.push(this.parseSequenceExpression()) }
-    while (this.eat("|"))
-    return {type: "choice", choices}
-  }
-
-  endOfSequence() {
-    return this.tokenType == "punctuation" &&
-      /^[|)}]$/.test(this.tokenValue)
-  }
-
-  parseSequenceExpression() {
-    let first = this.parseSubscriptExpression()
-    if (this.endOfSequence()) return first
-    let exprs = [first]
-    do { exprs.push(this.parseSubscriptExpression()) }
-    while (!this.endOfSequence())
-    return {type: "sequence", exprs}
-  }
-
-  parseSubscriptExpression() {
-    let expr = this.parsePrimaryExpression()
-
-    while (this.eat(":"))
-      expr = {type: "test", test: this.jsBlock(), expr}
-
-    for (;;) {
-      if (this.eat("*"))
-        expr = {type: "repeat", min: 0, max: -1, expr}
-      else if (this.eat("+"))
-        expr = {type: "repeat", min: 1, max: -1, expr}
-      else if (this.eat("?"))
-        expr = {type: "repeat", min: 0, max: 1, expr}
-      else
-        break
-    }
-
-    if (this.eat("!"))
-      expr = {type: "until", expr, until: this.parseExpression()}
-
+function parseExprInner(p) {
+  if (p.eat(tt.parenL)) {
+    let expr = parseExprChoice(p)
+    p.expect(tt.parenR)
     return expr
-  }
-
-  parsePrimaryExpression() {
-    if (this.tokenType == "string" || this.tokenType == "regexp") {
-      let value = {type: this.tokenType, value: this.tokenValue}
-      this.nextToken()
-      return value
+  } else if (p.type == tt.string) {
+    let node = p.startNode(), value = p.value
+    p.next()
+    if (p.type == tt.plusMin && p.value == "-") {
+      p.next()
+      if (p.type != tt.string) p.unexpected()
+      node.from = value
+      node.to = p.value
+      p.next()
+      return p.finishNode(node, "CharacterRange")
+    } else {
+      node.value = value
+      return p.finishNode(node, "StringMatch")
     }
-
-    if (this.tokenType == "identifier") {
-      let value = {type: "apply", rule: this.tokenValue}
-      this.nextToken()
-      if (this.eat("="))
-        return {type: "binding", name: value.rule, expr: this.parseExpression()}
-      value.args = this.is("[") ? this.jsArgs() : null
-      return value
-    }
-
-    if (this.eat("(")) {
-      let inside = this.parseExpression()
-      this.expect(")")
-      return inside
-    }
-
-    if (this.eat(":"))
-      return {type: "test", test: this.jsBlock(), expr: null}
-
-    this.raise("Unexpected token")
-  }
-
-  parseRule() {
-    let name = this.ident(), params = []
-    if (this.eat("[")) {
-      if (!this.is("]")) {
-        do { params.push(this.ident()) }
-        while (this.eat(","))
-      }
-      this.expect("]")
-    }
-    this.expect("{")
-    let expr = this.parseExpression(), body = null
-    this.expect("}")
-    if (this.is("{")) body = this.jsBlock()
-    return {name, expr, body, params}
-  }
-
-  parseRuleSet() {
-    let rules = []
-    while (!this.is("}")) rules.push(this.parseRule())
-    return rules
+  } else {
+    let node = p.startNode()
+    node.name = p.parseIdent(true)
+    return p.finishNode(node, "RuleIdentifier")
   }
 }
 
-function isNewline(ch) { return ch == "\n" || ch == "\r" }
+function parseExprSuffix(p) {
+  let start = p.start
+  let expr = parseExprInner(p)
+  if (p.type == tt.star || p.type == tt.question || p.type == tt.plusMin && p.value == "+") {
+    let node = p.startNodeAt(start)
+    node.expr = expr
+    node.repeat = p.value
+    p.next()
+    return p.finishNode(node, "RepeatedRule")
+  }
+  return expr
+}
 
-function identifierChar(ch) { return /\w/.test(ch) }
+function parseExprLookahead(p) {
+  if (p.type == tt.prefix && (p.value == "!" || p.value == "~")) {
+    let node = p.startNode()
+    node.type = p.value
+    p.next()
+    node.expr = parseExprSuffix(p)
+    return p.finishNode(node, "LookaheadRule")
+  } else {
+    return parseExprSuffix(p)
+  }
+}
+
+function endOfSequence(p) {
+  return p.type == tt.braceR || p.type == tt.parenR || p.type == tt.bitwiseOR
+}
+
+function parseExprSequence(p) {
+  let start = p.start, first = parseExprLookahead(p)
+  if (endOfSequence(p)) return first
+  let node = p.startNodeAt(start)
+  node.exprs = [first]
+  do { node.exprs.push(parseExprLookahead(p)) }
+  while (!endOfSequence(p))
+  return p.finishNode(node, "RuleSequence")
+}
+
+function parseExprChoice(p) {
+  let start = p.start, left = parseExprSequence(p)
+  if (!p.eat(tt.bitwiseOR)) return left
+  let node = p.startNodeAt(start)
+  node.choices = [left]
+  do { node.choices.push(parseExprSequence(p)) }
+  while (p.eat(tt.bitwiseOR))
+  return p.finishNode(node, "RuleChoice")
+}
+
+plugins.modeGrammar = function(parser) {
+  parser.extend("parseStatement", inner => function(decl, topLevel, exports) {
+    if (topLevel && decl && this.isContextual("grammar"))
+      return parseGrammar(this)
+    else
+      return inner.call(this, decl, topLevel, exports)
+  })
+}
+
+module.exports = function(file) {
+  return parse(file, {
+    plugins: {modeGrammar: true}
+  })
+}
