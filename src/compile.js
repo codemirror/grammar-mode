244bd16a85ddde6f83fa5a8d1e10f3e67c749c1c
Marijn Haverbeke <marijn@haverbeke.nl>
2017-04-27 15:07:49 +0200

    Refactor graph code

--- src/compile.js
+++ src/compile.js
@@ -8,6 +8,41 @@ module.exports = function(ast, file) {
   return result
 }
 
+class Graph {
+  constructor(grammar) {
+    this.nodes = Object.create(null)
+    this.curLabel = "-"
+    this.curLabelId = 0
+    this.rules = Object.create(null)
+    this.grammar = grammar
+    this.first = null
+    for (let i = 0; i < grammar.rules.length; i++) {
+      let rule = grammar.rules[i]
+      this.rules[rule.id.name] = {ast: rule, start: null, end: null}
+      if (this.first == null && !rule.lexical) this.first = rule.id.name
+    }
+    fillRule(this, this.rules[this.first])
+  }
+
+  node() {
+    let label = this.curLabel + this.curLabelId++
+    return this.nodes[label] = new Node(label)
+  }
+
+  merge(a, b) {
+    this.nodes[b.label] = a
+  }
+
+  withLabels(label, f) {
+    let prevLabel = this.curLabel, prevId = this.curLabelId
+    this.curLabel = label
+    this.curLabelId = 0
+    f()
+    this.curLabel = prevLabel
+    this.curLabelId = prevId
+  }
+}
+
 class Node {
   constructor(label) {
     this.label = label || getLabel()
@@ -27,7 +62,7 @@ class Edge {
   }
 
   toString() {
-    let effects = this.effects.map(e => e[0] + (e[1] ? " " + e[1] : "")).join(" ")
+    let effects = this.effects.map(e => e[0] + (e[1] ? " " + e[1].id.name : "")).join(" ")
     return `${this.from} -> ${this.to}[label=${JSON.stringify(this.match.toString() + (effects ? " " + effects : ""))}]`
   }
 
@@ -70,46 +105,13 @@ class NullMatch extends Match {
   toString() { return "ø" }
 }
 
-function createGraph(grammar) {
-  let rules = Object.create(null), first = null
-  for (let i = 0; i < grammar.rules.length; i++) {
-    let rule = grammar.rules[i]
-    rules[rule.id.name] = {ast: rule, graph: null}
-    if (first == null && !rule.lexical) first = rule.id.name
-  }
-  return getRuleGraph(rules, first)
-}
-
-let curLabel, curLabelId
-function withLabels(label, f) {
-  let prevLabel = curLabel, prevId = curLabelId
-  curLabel = label
-  curLabelId = 0
-  f()
-  curLabel = prevLabel
-  curLabelId = prevId
-}
-
-function getLabel() {
-  return curLabel + curLabelId++
-}
-
-function buildRuleGraph(rule, rules) {
-  withLabels(rule.ast.id.name, () => {
-    let start = new Node, end = new Node
-    rule.graph = {start, end}
-    buildExprGraph(start, end, rule.ast.expr, rules)
+function fillRule(graph, rule) {
+  graph.withLabels(rule.ast.id.name, () => {
+    fillExpr(rule.start = graph.node(), rule.end = graph.node(), rule.ast.expr, graph)
   })
 }
 
-function getRuleGraph(rules, name) {
-  let rule = rules[name]
-  if (!rule) throw new SyntaxError("Rule '" + name + "' is not defined")
-  if (!rule.graph) buildRuleGraph(rule, rules)
-  return rule.graph
-}
-
-function buildExprGraph(start, end, expr, rules) {
+function fillExpr(start, end, expr, graph) {
   let t = expr.type
   if (t == "CharacterRange") {
     Edge.create(start, end, new RangeMatch(expr, expr.from, expr.to))
@@ -118,56 +120,69 @@ function buildExprGraph(start, end, expr, rules) {
   } else if (t == "AnyMatch") {
     Edge.create(start, end, new AnyMatch(expr))
   } else if (t == "RuleIdentifier") {
-    let subGraph = getRuleGraph(rules, expr.id.name)
-    Edge.create(start, subGraph.start, new NullMatch(expr), [["call", expr.id.name, end]])
-    Edge.create(subGraph.end, end, new NullMatch(expr), [["return"]])
+    let rule = graph.rules[expr.id.name]
+    if (!rule.start) fillRule(graph, rule)
+    Edge.create(start, graph.nodes[rule.start], new NullMatch(expr), [["call", rule.ast, end]])
+    Edge.create(graph.nodes[rule.end], end, new NullMatch(expr), [["return"]])
   } else if (t == "RepeatedMatch") {
     if (expr.kind == "*") {
       Edge.create(start, end, new NullMatch(expr))
-      buildExprGraph(start, start, expr.expr, rules)
+      fillExpr(start, start, expr.expr, graph)
     } else if (expr.kind == "+") {
-      buildExprGraph(start, end, expr.expr, rules)
-      buildExprGraph(end, end, expr.expr, rules)
+      fillExpr(start, end, expr.expr, graph)
+      fillExpr(end, end, expr.expr, graph)
     } else if (expr.kind == "?") {
       Edge.create(start, end, new NullMatch(expr))
-      buildExprGraph(start, end, expr.expr, rules)
+      fillExpr(start, end, expr.expr, graph)
     }
   } else if (t == "LookaheadMatch") {
     throw new Error("not supporting lookahead yet")
   } else if (t == "SequenceMatch") {
     for (let i = 0; i < expr.exprs.length; i++) {
-      let to = i == expr.exprs.length - 1 ? end : new Node
-      buildExprGraph(start, to, expr.exprs[i], rules)
+      let to = i == expr.exprs.length - 1 ? end : graph.node()
+      fillExpr(start, to, expr.exprs[i], graph)
       start = to
     }
   } else if (t == "ChoiceMatch") {
     for (let i = 0; i < expr.exprs.length; i++)
-      buildExprGraph(start, end, expr.exprs[i], rules)
+      fillExpr(start, end, expr.exprs[i], graph)
   } else {
     throw new Error("Unrecognized AST node type " + t)
   }
 }
 
-function printGraph(node) {
-  let seen = Object.create(null)
-  seen[node.label] = true
-  let toScan = [node], output = "digraph {\n"
-  while (toScan.length) {
-    let cur = toScan.pop()
-    for (let i = 0; i < cur.outgoing.length; i++) {
-      let edge = cur.outgoing[i]
-      output += "  " + edge.toString() + ";\n"
-      if (!(edge.to.label in seen)) {
-        seen[edge.to.label] = true
-        toScan.push(edge.to)
-      }
-    }
+function printGraph(graph) {
+  let output = "digraph {\n"
+  for (let n in graph.nodes) {
+    let node = graph.nodes[n]
+    for (let i = 0; i < node.outgoing.length; i++)
+      output += "  " + node.outgoing[i].toString() + ";\n"
   }
   return output + "}\n"
 }
 
-function compileGrammar(node, file) {
-  let graph = createGraph(node)
-  console.log(printGraph(graph.start))
+function simplifySequence(node) {
+  return false
+}
+
+function simplifyChoice(node) {
+}
+
+function simplifyCall(node) {
+}
+
+function simplifyNull(node) {
+
+}
+
+// Look for simplification possibilities around the given node, return
+// true if anything was done
+function simplify(node) {
+  return simplifySequence(node) || simplifyChoice(node) || simplifyCall(node) || simplifyNull(node)
+}
+
+function compileGrammar(grammar, file) {
+  let graph = new Graph(grammar)
+  console.log(printGraph(graph))
   return "FIXME"
 }
